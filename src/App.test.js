c473897a0c169c86b63159e15a0a2fe66420af2f
David Preece <david.preece08@gmail.com>
2020-02-19 18:43:22 +0000

    Update App tests
    Updates the App component tests from the default CRA ones to related ones.

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,27 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { createShallow } from '@material-ui/core/test-utils';
 import App from './App';
+import PageHeader from './components/PageHeader';
+import PageContent from './components/PageContent';
+import { CssBaseline } from '@material-ui/core';
 
-test('renders learn react link', () => {
-    const { getByText } = render(<App />),
-        linkElement = getByText(/learn react/i);
+describe('renders learn react link', () => {
+    let shallow, wrapper;
 
-    expect(linkElement).toBeInTheDocument();
+    beforeAll(() => {
+        shallow = createShallow();
+        wrapper = shallow(<App />);
+    });
+
+    it('should render the PageHeader component', () => {
+        expect(wrapper.find(PageHeader).length).toBe(1);
+    });
+
+    it('should render the PageContent component', () => {
+        expect(wrapper.find(PageContent).length).toBe(1);
+    });
+
+    it('should render the CssBaseline component', () => {
+        expect(wrapper.find(CssBaseline).length).toBe(1);
+    });
 });
