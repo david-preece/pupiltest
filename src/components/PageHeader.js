f5e6a052d28f95dd2144c0cf5525891ce221abbd
David Preece <david.preece08@gmail.com>
2020-02-19 19:25:14 +0000

    Update PageHeader to use store
    Updates the PageStore component to use the application store for the page title

--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -1,15 +1,12 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { AppBar, Toolbar, IconButton, Typography, Menu, MenuItem } from '@material-ui/core';
+import { AppBar, IconButton, Menu, MenuItem, Toolbar, Typography } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
+import { observer } from 'mobx-react';
+import React, { Component } from 'react';
+import { ApplicationStoreContext } from '../stores/ApplicationStore';
 
-export default class PageHeader extends Component {
+class PageHeader extends Component {
     state = {
         anchorEl: null,
-    };
-
-    static propTypes = {
-        prop: PropTypes,
     };
 
     handleCloseMenu = () => this.setState({ anchorEl: null });
@@ -17,6 +14,8 @@ export default class PageHeader extends Component {
     handleOpenMenu = event => this.setState({ anchorEl: event.currentTarget });
 
     render() {
+        const store = this.context;
+
         return (
             <AppBar position="static">
                 <Toolbar>
@@ -43,9 +42,13 @@ export default class PageHeader extends Component {
                         <MenuItem onClick={this.handleCloseMenu}>Add Property</MenuItem>
                         <MenuItem onClick={this.handleCloseMenu}>Clear Properties</MenuItem>
                     </Menu>
-                    <Typography variant="h6">Page Header</Typography>
+                    <Typography variant="h6">{store.pageTitle}</Typography>
                 </Toolbar>
             </AppBar>
         );
     }
 }
+
+PageHeader.contextType = ApplicationStoreContext;
+
+export default observer(PageHeader);
